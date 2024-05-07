a2280041095bfae6e028fdbe007b645ce817838f
Haruaki <misbahulk2236@gmail.com>
2024-05-08 00:37:11 +0700

    fix: listen port log

--- index.js
+++ index.js
@@ -416,6 +416,7 @@ api.use(middlewares.errorHandler)
 
 app.use("/api/v1", api)
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server listening on :3000")
+const port = +process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server listening on :"+port);
 })
